5bc0b53e1bc5001635681fc0bb95710d32db0474
Akshay Chaturvedi <achaturvedi332@gmail.com>
2018-10-10 18:10:02 +0530

    StudentSQLite

--- Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/DatabaseHelper.java
+++ Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/DatabaseHelper.java
@@ -1,14 +1,21 @@
 package com.example.studentadmin.studentsqlite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper  extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "student.db";
+    public static final String col1 = "id";
+    public static final String col2 = "Fname";
+    public static final String col3 = "Lname";
+    public static final String col4 = "marks";
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -19,5 +26,43 @@ public class DatabaseHelper  extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE if exists student_Details");
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(int id, String FName, String LName,int marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(col1,id);
+        cv.put(col2,FName);
+        cv.put(col3,LName);
+        cv.put(col4,marks);
+
+        db.insert("student_Details",null,cv);
+        return true;
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM student_Details",null);
+        return res;
+    }
+
+    public Integer deleteData(Integer id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Integer deleteRows = db.delete("student_Details","id=?",new String[]{id.toString()});
+        return deleteRows;
+    }
+
+    public boolean updateData(Integer id, String FName, String LName, Integer marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(col1,id);
+        cv.put(col2,FName);
+        cv.put(col3,LName);
+        cv.put(col4,marks);
+        db.update("student_Details",cv,"id=?",new String[]{id.toString()});
+
+        return true;
     }
 }
--- Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/MainActivity.java
+++ Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.studentadmin.studentsqlite;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper dbh;
+    EditText Sid, Firstname, Lastname, Smarks;
+    Button Binsert, Bupdate, Bdelete, Bview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbh = new DatabaseHelper(this);
+
+        Sid = findViewById(R.id.id);
+        Firstname = findViewById(R.id.FName);
+        Lastname = findViewById(R.id.LName);
+        Smarks = findViewById(R.id.marks);
+
+        Binsert = findViewById(R.id.InsertData);
+        Bupdate = findViewById(R.id.UpdateData);
+        Bdelete = findViewById(R.id.DeleteData);
+        Bview = findViewById(R.id.ViewData);
+
+        Binsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = dbh.insertData(Integer.parseInt(Sid.getText().toString()), Firstname.getText().toString(), Lastname.getText().toString(), Integer.parseInt(Smarks.getText().toString()));
+
+                if (isInserted == true){
+                    Toast.makeText(getApplicationContext(),"Data Inserted Successfuly",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Bupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isUpdated = dbh.updateData(Integer.parseInt(Sid.getText().toString()),Firstname.getText().toString(),Lastname.getText().toString(),Integer.parseInt(Smarks.getText().toString()));
+                if(isUpdated){
+                    Toast.makeText(getApplicationContext(),"Data Updated",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Data Not Updated",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Bdelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer deleteRows = dbh.deleteData(Integer.parseInt(Sid.getText().toString()));
+                if(deleteRows > 0){
+                    Toast.makeText(getApplicationContext(),"Data Deleted",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Data Not Deleted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Bview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = dbh.getData();
+                if(res.getCount() == 0){
+
+                }
+                else {
+                    StringBuffer sbf = new StringBuffer();
+                    while (res.moveToNext()){
+                        sbf.append("\nID: "+res.getInt(0)+
+                                    "\nFirst Name: "+res.getString(1)+
+                                    "\nLast Name: "+res.getString(2)+
+                                    "\nMarks: "+res.getInt(3));
+                    }
+                    showMessage("Data",sbf.toString());
+                }
+            }
+        });
+
+    }
+    public void showMessage(String title, String msg){
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setCancelable(true);
+        adb.setTitle(title);
+        adb.setMessage(msg);
+        adb.show();
     }
 }

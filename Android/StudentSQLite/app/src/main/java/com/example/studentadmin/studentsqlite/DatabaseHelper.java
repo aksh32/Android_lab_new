da25f5f24f69c8a7daa2d891c25d2b0f4eaa44f7
Akshay Chaturvedi <achaturvedi332@gmail.com>
2018-10-10 15:55:29 +0530

    build

--- Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/DatabaseHelper.java
+++ Android/StudentSQLite/app/src/main/java/com/example/studentadmin/studentsqlite/DatabaseHelper.java
@@ -5,17 +5,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper  extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "student.db";
+
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase .execSQL("CREATE TABLE student_Details  (id integer PRIMARY KEY, Fname text, Lname text, marks int)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE if exists student_Details");
     }
 }
